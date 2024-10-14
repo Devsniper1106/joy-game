8528ad84b0ff554004d585d696955c824dd16d91
Devsniper1106 <andyblake1106@gmail.com>
2024-10-14 16:20:41 +0800

    fix: code structure

--- src/lib/types/api/model/game.d.ts
+++ src/lib/types/api/model/game.d.ts
@@ -1,13 +1,13 @@
 export type TGameItemDto = {
-  id: string;
+  id: string
   /** The name of the game */
-  name: string;
+  name: string
   /** The url of the game package */
-  link_url: string;
+  link_url: string
   /** The url of the game icon image */
-  icon_url: string;
+  icon_url: string
   /** When true, the icon image shoult be shown in large size */
-  is_hot?: boolean;
-  created_at: string;
-  updated_at: string;
-};
+  is_hot?: boolean
+  created_at: string
+  updated_at: string
+}
