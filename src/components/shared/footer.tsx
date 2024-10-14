6df44e74172af67c3519bcfa923172631a740224
Devsniper1106 <andyblake1106@gmail.com>
2024-10-15 05:39:53 +0800

    fix(H5G-13):responsiveness for footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="mt-auto h-10  bg-[#422006] text-sm w-full">
-      <section className="flex flex-wrap p-2 justify-center gap-4 sm:gap-12 text-white">
+    <footer className="my-auto h-8 sm:h-10  bg-[#422006] text-[8px] sm:text-sm w-full">
+    <section className="flex flex-wrap items-center py-2 justify-center gap-4 sm:gap-12 text-white">
+     
         <div>Copyright © 2024 All rights Reserved</div>
         <Link href="/service">Terms of service</Link>
         <Link href="/privacy">Privacy policy</Link>
