c0f48c9e770a19042a406639b573886cb71691e9
Devsniper1106 <andyblake1106@gmail.com>
2024-10-15 05:49:01 +0800

    fix(H5G):improve api response

--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -9,77 +9,56 @@ export const Menu = () => {
   const router = useRouter()
   const { theme, setTheme } = useTheme()
 
-  // Close the dropdown munu when clicking the screen.
-  // window.onclick = function (e) {
-  //   const modal = document.getElementById('theme')
-  //   if (modal != e.target) {
-  //     setShow(false)
-  //   }
-  //   console.log(modal != e.target)
-  // }
+  const accountBtn = useRef<HTMLDivElement | null>(null)
 
-  const accountBtn = useRef();
-  const handleClickOutside = (event:any) => {
-    // Check if the clicked element is outside the list
-    if (accountBtn.current && !accountBtn.current.contains(event.target)) {
-      setShow(false);
+  const handleClickOutside = (event: MouseEvent) => {
+    // Check if the clicked element is outside the dropdown menu
+    if (
+      accountBtn.current &&
+      !accountBtn.current.contains(event.target as Node)
+    ) {
+      setShow(false)
     }
-  };
+  }
+
   useEffect(() => {
     // Add event listener for clicks
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside)
     // Cleanup the event listener on component unmount
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <>
-      <div className="relative sm:hidden flex flex-col ">
-        {/* <img
-          id="theme"
-          src={`${theme === 'dark' ? 'menu2' : 'menu'}.svg`}
-          className="cursor-pointer"
-          onClick={() => setShow(!show)}
-        /> */}
-        <button  onClick={() => setShow(!show)}>
-          <MenuIcon />
-        </button>
-        {show && (
+    <div className="relative sm:hidden flex flex-col" ref={accountBtn}>
+      <button onClick={() => setShow(!show)}>
+        <MenuIcon />
+      </button>
+      {show && (
+        <div
+          className={`absolute top-16 -right-2 w-28 flex flex-col gap-2 ${
+            theme === 'dark'
+              ? 'text-white bg-[#1E2854]'
+              : 'text-[#0C4A6E] bg-[#CEDAED]'
+          } text-[18px] rounded-sm p-2 z-30`}
+        >
           <div
-          
-            className={`absolute top-16 -right-2 w-28 flex flex-col gap-2 ${
-              theme === 'dark' ? 'text-white bg-[#1E2854]':'text-[#0C4A6E] bg-[#CEDAED] '
-            }  text-[18px] rounded-sm p-2 z-30`}
-            onClick={() => setShow(false)}
+            className="flex gap-2 cursor-pointer"
+            onClick={() => router.push('/')}
           >
-            <div
-              className="flex gap-2 cursor-pointer"
-              onClick={() => router.push('/')}
-            >
-              {/* {theme === 'dark' ? (
-                <img src="home2.svg" />
-              ) : (
-                <img src="home.svg" />
-              )} */}
-              <HomeIcon />
-              <span>Home</span>
-            </div>
-            <div
-              className="flex gap-2 cursor-pointer"
-              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            >
-              {/* {theme === 'dark' ? (
-                <img src="light2.svg" />
-              ) : (
-                <img src="light.svg" />
-              )} */}
-              <ThemeIcon />
-              <span>{theme === 'dark' ? 'Dark' : 'Light'}</span>
-            </div>
+            <HomeIcon />
+            <span>Home</span>
           </div>
-        )}
-      </div>
-    </>
+          <div
+            className="flex gap-2 cursor-pointer"
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+            <ThemeIcon />
+            <span>{theme === 'dark' ? 'Dark' : 'Light'}</span>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
