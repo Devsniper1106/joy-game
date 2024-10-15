ce2a8bdba95adf077b238c76614c1508da643ea4
Devsniper1106 <andyblake1106@gmail.com>
2024-10-15 12:42:44 +0800

    release: v0.1.0-alpha.0 on 2024-10-15

--- src/app/details/[cards]/page.tsx
+++ src/app/details/[cards]/page.tsx
@@ -1,14 +1,13 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import Card from '@/components/ui/card'
-import { useTheme } from 'next-themes'
-import Link from 'next/link'
-import { TGameItemDto } from '@/lib/types/api/model/game'
 import { getGameList } from '@/actions/getGameList'
+import Card from '@/components/ui/card'
+import { TGameItemDto } from '@/lib/types/api/model/game'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { FaGamepad } from 'react-icons/fa'
 
 const Page = () => {
-  const { theme } = useTheme()
   const [gameItems, setGameItems] = useState<TGameItemDto[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string | null>(null)
@@ -60,21 +59,41 @@ const Page = () => {
         <div className="w-full">
           <div className="flex sm:h-[300px] lg:min-w-[600px] sm:max-w-[800px] gap-5 px-[15px] py-[31px] sm:py-[45px] sm:px-[66px] dark:bg-[#0206177A] bg-[#9FB0FD7A] rounded-[36px]">
             <div className="w-1/2 flex align-middle">
-              <Card icon_url={`${selectedGameItem?.icon_url}`} name={`${selectedGameItem?.name}`} />
+              <Card
+                icon_url={`${selectedGameItem?.icon_url}`}
+                name={`${selectedGameItem?.name}`}
+              />
             </div>
             <div>
-              <div className="font-medium text-[20px] sm:text-[32px] mb-8">{endpoint}</div>
-              <Link href={`/play/${selectedGameItem?.name}`} className="flex w-fit items-center px-4 sm:px-12 py-1 sm:py-3 gap-2 sm:gap-6 rounded-[36px] outline-4 outline-[#A3E635] dark:bg-[#6366F14D] bg-[#9FB0FD7A]">
-                <div className="font-extrabold text-[20px] sm:text-[32px]">Play</div>
+              <div className="font-medium text-[20px] sm:text-[32px] mb-8">
+                {endpoint}
+              </div>
+              <Link
+                href={`/play/${selectedGameItem?.name}`}
+                className="flex w-fit items-center px-4 sm:px-12 py-1 sm:py-3 gap-2 sm:gap-6 rounded-[36px] border-4 border-amber-500 dark:border-lime-400 bg-[#6366F1]/30"
+              >
+                <FaGamepad className="text-[20px] sm:text-[32px]" />
+                <div className="font-extrabold text-[20px] sm:text-[32px]">
+                  Play
+                </div>
               </Link>
             </div>
           </div>
         </div>
         <div className="px-3 py-3 max-w-[600px] min-w-200">
-          <div className="font-extrabold text-[16px] sm:text-[28px] pb-6 sm:pb-12">Enjoy Other Games</div>
+          <div className="font-extrabold text-[16px] sm:text-[28px] pb-6 sm:pb-12">
+            Enjoy Other Games
+          </div>
           <div className="grid grid-cols-3 grid-flow-dense gap-x-3 gap-y-2 sm:gap-x-8 sm:gap-y-4 lg:grid-cols-2 xl:grid-cols-3">
             {gameItems.map((card) => (
-              <div key={card.id} className={`${card.is_hot ? 'col-span-2 row-span-2' : 'col-span-1 row-span-1'}`}>
+              <div
+                key={card.id}
+                className={`${
+                  card.is_hot
+                    ? 'col-span-2 row-span-2'
+                    : 'col-span-1 row-span-1'
+                }`}
+              >
                 <Card icon_url={card.icon_url} name={card.name} />
               </div>
             ))}
